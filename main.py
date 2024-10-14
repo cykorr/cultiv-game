07ee66268b07043000c22ac5ee31cc76e38722ff
korr <pyk0rr@gmail.com>
2024-10-14 18:28:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ created by the mighty heydon''')
 import random
 import time
 
+living_status = 1 # 1 is alive 0 is dead 0.5 is zombie like
 cultivate_chance = 0
 stats_strength = 0
 stats_realm = 0 
 stats_wealth = 0
 power_type = "neutral"
+time_power = 5
 # ---------------------------
 character_name = str(input('''what do u want ur name to be?
 '''))
@@ -29,14 +31,18 @@ b) immortal cultivation
 time.sleep(4.0)
 
 if choice_1.lower() == "a":
-    print('''you have chosen... mortal life
-ibsfr thats lowkey boring.
-you live a boring and uneventful life until you die at the ripe old age of 40
+    print('''you have chosen... mortal life''')
+    time.sleep(1)
+    print("ibsfr thats lowkey boring.")
+    time.sleep(1)
+    print('''you live a boring and uneventful life until you die at the ripe old age of 40
 due to the shockwaves of a fight among immortals''')
+    living_status = 0
+    quit()
 # END 
 
 if choice_1.lower() == "b":
-    power_type = input('''IMMORTAL : you have chosen... IMMORTAL CULTIVATION
+    power_type = input('''IMMORTAL (cannot determine) : you have chosen... IMMORTAL CULTIVATION
 what path do you want to travel on?
 a) time path
 b) space path
@@ -53,12 +59,16 @@ d) mind
         power_type = "mind"
     else:
         print("you have failed to answer and died due to the backlash of summoning an immortal")
+        living_status = 0
+        quit()
+
+time.sleep(5)
 
 choice_daily = input('''what do u want to do now?
 [cultivate,explore,kys or engage in thievery]
 ''')
 
-if choice_daily.lower() == cultivate :
+if choice_daily.lower() == "cultivate" :
     print("nerd")
     print('''Immortal : Immortal ranks are in this order :
 100 is Realm 1 Immortal
@@ -73,24 +83,35 @@ after realm 5, you are not qualified to know.''')
     print("you have cultivated to",stats_realm)
     cultivate_chance = 0
 
-elif choice_daily.lower() == explore:
-    print("REALM 3 GUARD : your realm is currently too low to explore but I'll let u go")
-    print("you encounter a REALM 1 Wild Boar! 100/100 HP")
+elif choice_daily.lower() == "explore":
+    print("REALM 3 GUARD (neutral) : your realm is currently too low to explore but I'll let u go")
+    print("you encounter a REALM 1 Wild Boar! 100/100 HP (enemy)")
     fight_choice = input("fight? y/n")
-    if fight_choice.lower() == y:
+    if fight_choice.lower() == "y" :
         print('''you could not manage to cross realms to fight and as a result... 
 you died to the REALM 1 Wild Boar''')
         if power_type == "time":
-        print("due to your time power, you have been reborn.")
+            print("due to your time power, you have been reborn.")
+        else:
+            living_status = 0
+            quit()
     else:
         print("you took too long and died 💀☠️☠️")
         if power_type == "time":
-        print("due to your time power, you have been reborn. don't make the same mistake again!")
+            print("due to your time power, you have been reborn. don't make the same mistake again!")
+            time_power = time_power - 1
+
+elif choice_daily.lower() == "kys":
+    print("you decided this decision was too overwhelming and exploded your soul")
+    living_status = 0
+    quit()
 
 
 
 
 
+elif living_status == 0:
+    print("you are currently a ghost.")
 
 
 
